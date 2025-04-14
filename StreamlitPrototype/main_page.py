f91ba701a85fce6a502385c7437fcf7cded21bb8
leontn121 <leonnguyen121@gmail.com>
2025-04-13 20:00:41 -0700

    Add files via upload

---
+++ StreamlitPrototype/main_page.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+
+
+st.markdown("# Main page ")
+st.sidebar.markdown("# Main page ")
+
+message = "Every second counts in an emergency. Our project is designed to support 911 operators by simplifying and accelerating the transcription process of emergency calls. Using intelligent speech-to-text technology and real-time language processing, the Emergency Transcription Assistant helps convert spoken conversations into clear, searchable text—reducing cognitive load, minimizing errors, and ensuring accurate records for dispatchers and emergency responders. With features like speaker identification, time-stamped entries, and keyword highlighting, our solution enables operators to stay focused on what matters most: saving lives. Whether it's high-stress situations, overlapping voices, or fast-paced information flow, our tool is built to handle the complexity and urgency of 911 communication—streamlining documentation without compromising clarity or speed."
+st.write(message)
+
+
